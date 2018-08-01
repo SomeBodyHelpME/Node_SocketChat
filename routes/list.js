3095baa4de46e0b3456eaf4e89cf9b3c53da2779
Sangeun Lee <dltahazh@gmail.com>
2018-08-01 13:22:29 +0900

    directory change

--- routes/list.js
+++ routes/list.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const sql = require('../module/sql.js');
-const jwt = require('../../module/jwt.js');
+const jwt = require('../module/jwt.js');
 
 router.get('/:g_idx', async(req, res) => {
 	let token = req.headers.token;
