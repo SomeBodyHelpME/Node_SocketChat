d23c983ba5acb34df6022add67fe9264f9b36407
Sangeun Lee <dltahazh@gmail.com>
2018-08-23 21:04:14 +0900

    clientCount test

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ root_io.of(/\/(\d+)$/).on('connection', function (socket) {
 		// console.log("before enter socket.conn.server.clientsCount : ", socket.conn.server.clientsCount);
 		console.log("before userlist : ", socket.userlist);
 		socket.join(chatroom_idx);
-		
+
 		// console.log("after enter socket.conn.server.clientsCount : ", socket.conn.server.clientsCount);
 		socket.room = chatroom_idx;
 		
@@ -143,7 +143,7 @@ root_io.of(/\/(\d+)$/).on('connection', function (socket) {
 		console.log("after userlist : ", socket.userlist);
 		let result = await chatsql.enterChatroom(u_idx, chatroom_idx);
 		let result2 = await chatsql.showAllMessage(u_idx, chatroom_idx);
-
+		console.log("socket.server.engine.clientsCount : ", socket.server.engine.clientsCount);
 		console.log("enterroom result : ", socket.conn.server.clientsCount);
 		if (result) {
 			root_io.of(newNsp.name).in(chatroom_idx).emit('roomresult', result2);
